b9cf7baf0d270cc8fe48f22d67cbadb53db7a9db
Jeff Lopes <jlopes@monkeycity.org>
2014-06-10 23:25:02 -0400

    gtfs_min works now

--- gtfs_min.rb
+++ gtfs_min.rb
@@ -3,19 +3,19 @@
 require 'sqlite3'
 require 'fileutils'
 
-def getStopsForRoute(route, direction) 
-    puts @old_db.execute <<-eos
-    	SELECT '#{route}','#{direction}',stop_times.stop_id,stop_times.stop_sequence,stops.stop_name,stops.stop_lat,stops.stop_lon
+def getStopsForRoute(route, direction)
+    stops = @old_db.execute <<-eos
+    	SELECT stop_times.stop_id,stop_times.stop_sequence
 		FROM stop_times 
 		INNER JOIN stops  
 		ON stop_times.stop_id=stops.stop_id 
 		WHERE stop_times.trip_id IN (SELECT trip_id FROM trips WHERE route_id = '#{route}' AND direction_id = '#{direction}') 
-		GROUP BY stop_times.stop_id,stop_times.stop_sequence
+		GROUP BY stop_times.stop_id,stop_times.stop_sequence;
     eos
 end
 
 def getRoutes
-	routes = @old_db.execute "select route_id from routes"
+	routes = @old_db.execute "select route_id from routes;"
 end
 
 begin
@@ -27,12 +27,42 @@ begin
 	@old_db = SQLite3::Database.open @orig_db_name
 	@new_db = SQLite3::Database.open @min_db_name
 
-	@new_db.execute "DROP TABLE calendar"
-	@new_db.execute "DROP TABLE fare_attributes"
-	@new_db.execute "DROP TABLE fare_rules"
-	@new_db.execute "VACUUM"
+	@old_db.results_as_hash = true
+	@new_db.results_as_hash = true
 
-	puts getStopsForRoute("933_", "1")
+	@new_db.execute <<-eos
+		CREATE TABLE route_stops(
+			route_id CHAR(11) NOT NULL,
+   			direction_id TINYINT(1) NOT NULL,
+   			stop_id CHAR(11) NOT NULL,
+   			stop_sequence INT(11) NOT NULL
+		);
+	eos
+
+	routes = getRoutes
+	routes.each do |route|
+		for i in 0..1
+			route_id = route['route_id']
+			stops = getStopsForRoute(route_id, i.to_s)
+			
+			stops.each do |stop|
+				@new_db.execute <<-eos
+					INSERT INTO route_stops 
+						(route_id, direction_id, stop_id, stop_sequence) 
+					VALUES 
+						('#{route_id}', #{i.to_s}, '#{stop['stop_id']}', #{stop['stop_sequence']});
+				eos
+			end
+		end
+	end
+
+	@new_db.execute "DROP TABLE calendar;"
+	@new_db.execute "DROP TABLE fare_attributes;"
+	@new_db.execute "DROP TABLE fare_rules;"
+	@new_db.execute "DROP TABLE stop_times;"
+	@new_db.execute "DROP TABLE trips;"
+	@new_db.execute "VACUUM;"
+	
 rescue SQLite3::Exception => e 
     puts "Exception occured"
     puts e	 
